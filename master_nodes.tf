6d88b014392257388705b00aa6a253a173953c50
John B <140195464+biehl-j@users.noreply.github.com>
2023-08-04 13:12:42 -0500

    Update master_nodes.tf

--- master_nodes.tf
+++ master_nodes.tf
@@ -24,6 +24,7 @@ resource "proxmox_vm_qemu" "k3s-master" {
   clone = var.node_template
 
   pool = var.proxmox_resource_pool
+  scsihw = var.scsihw
 
   # cores = 2
   cores   = var.master_node_settings.cores
