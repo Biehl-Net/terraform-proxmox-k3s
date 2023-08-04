1bfd9a0793bb9380778bd709f8f4c6401768ff94
John B <140195464+biehl-j@users.noreply.github.com>
2023-08-04 12:38:43 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -62,17 +62,17 @@ variable "proxmox_resource_pool" {
 
 variable "support_node_settings" {
   type = object({
-    cores          = optional(number),
-    sockets        = optional(number),
-    memory         = optional(number),
-    storage_type   = optional(string),
-    storage_id     = optional(string),
-    disk_size      = optional(string),
-    user           = optional(string),
-    db_name        = optional(string),
-    db_user        = optional(string),
-    network_bridge = optional(string),
-    network_tag    = optional(number), 
+    cores          = optional(number, 2),
+    sockets        = optional(number, 1),
+    memory         = optional(number, 2408),
+    storage_type   = optional(string, "scsi"),
+    storage_id     = optional(string, "local-lvm"),
+    disk_size      = optional(string, "20G"),
+    user           = optional(string, "k3s"),
+    db_name        = optional(string, "k3s"),
+    db_user        = optional(string, "k3s"),
+    network_bridge = optional(string, "vmbr0"),
+    network_tag    = optional(number, -1), 
   })
 }
 
@@ -84,15 +84,15 @@ variable "master_nodes_count" {
 
 variable "master_node_settings" {
   type = object({
-    cores          = optional(number),
-    sockets        = optional(number),
-    memory         = optional(number),
-    storage_type   = optional(string),
-    storage_id     = optional(string),
-    disk_size      = optional(string),
-    user           = optional(string),
-    network_bridge = optional(string),
-    network_tag    = optional(number),
+    cores          = optional(number, 2),
+    sockets        = optional(number, 1),
+    memory         = optional(number, 2048),
+    storage_type   = optional(string, "scsi"),
+    storage_id     = optional(string, "local-lvm"),
+    disk_size      = optional(string, "20G"),
+    user           = optional(string, "k3s"),
+    network_bridge = optional(string, "vmbr0"),
+    network_tag    = optional(number, -1),
   })
 }
 
@@ -106,18 +106,16 @@ variable "node_pools" {
 
     taints = optional(list(string)),
 
-    cores        = optional(number),
-    sockets      = optional(number),
-    memory       = optional(number),
-    storage_type = optional(string),
-    storage_id   = optional(string),
-    disk_size    = optional(string),
-    user         = optional(string),
-    network_tag  = optional(number),
-
+    cores        = optional(number, 2),
+    sockets      = optional(number, 1),
+    memory       = optional(number, 2048),
+    storage_type = optional(string, "scsi"),
+    storage_id   = optional(string, "local-lvm"),
+    disk_size    = optional(string, "20G"),
+    user         = optional(string, "k3s"),
+    network_tag  = optional(number, -1),
+    network_bridge = optional(string, "vmbr0"),
     template = optional(string),
-
-    network_bridge = optional(string),
   }))
 }
 variable "api_hostnames" {
